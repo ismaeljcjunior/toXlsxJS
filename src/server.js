e1ec43bf4c25371b3aa9ce4ae13c82cf4896f024
Ismael Junior <ismaeljcjunior@gmail.com>
2023-01-30 17:11:54 -0300

    feat

--- src/server.js
+++ src/server.js
@@ -1,36 +1,52 @@
 const path = require("path");
 require("dotenv").config({ path: path.resolve(__dirname, "../.env") });
-
-const xl = require("excel4node");
-const wb = new xl.Workbook();
-const ws = wb.addWorksheet("nome planilha");
+const express = require("express");
+const app = express();
+const excel4node = require("excel4node");
 const dbMysql = require("./config/dbMysql");
 const dbMssql = require("./config/dbMssql");
 
-const testJsonToXlsx = async () => {
-  const headerNames = new Set()
-  let rouIndex = 2;
+app.get("/dow", (req, res) => {
+  const testJsonToXlsx = async () => {
+    const workbook = new excel4node.Workbook();
+    const worksheet = workbook.addWorksheet("Sheet 1");
 
-  const resultSQL = await dbMssql.query(process.env.SELECT_BASE, {
-    type: dbMssql.QueryTypes.SELECT,
-  });
-  resultSQL.forEach((obj) => { Object.keys(obj).forEach((key) => {headerNames.add(key)}) })
+    const resultSQL = await dbMssql.query(process.env.SELECT_BASE, {
+      type: dbMssql.QueryTypes.SELECT,
+    });
 
-  console.log(headerNames);
-};
-testJsonToXlsx();
+    const headers = Object.keys(resultSQL[0]);
 
-// const headingColumNanes = ["nome", "email", "phone"];
-// let headingColumNanesIndex = 1;
-// headingColumNanes.forEach((heading) => {
-//   ws.cell(1, headingColumNanesIndex++).string(heading);
-// });
-// let rowIndex = 2;
-// data.forEach((record) => {
-//   let columnIndex = 1;
-//   Object.keys(record).forEach((columnName) => {
-//     ws.cell(rowIndex, columnIndex++).string(record[columnName]);
-//   });
-//   rowIndex++;
-// });
-// wb.write('teste.xlsx')
+    headers.forEach((header, index) => {
+      worksheet.cell(1, index + 1).string(header);
+    });
+    resultSQL.forEach((data, dataIndex) => {
+      headers.forEach((header, headerIndex) => {
+        const value = data[header];
+        if (typeof value === "number" || typeof value === 'object') {
+          worksheet
+            .cell(dataIndex + 2, headerIndex + 1)
+            .string(JSON.stringify(value));
+        } else {
+          worksheet.cell(dataIndex + 2, headerIndex + 1).string(value);
+        }
+      });
+    });
+
+    res.setHeader(
+      "Content-Type",
+      "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+    );
+    res.setHeader("Content-Disposition", "attachment; filename=data.xlsx");
+
+
+    workbook.write("data.xlsx", res);
+
+    console.log(resultSQL);
+  };
+  testJsonToXlsx();
+});
+
+app.listen(3000, () => {
+  console.log("Server listening on port 3000");
+});
